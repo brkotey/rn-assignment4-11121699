fe39727ada8d382fa260e5d49a360bae7f3de262
brkotey <brkotey@st.ug.edu.gh>
2024-06-23 23:10:51 +0000

    Update HomeScreen.js

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,34 +1,34 @@
 import React from 'react';
 import { View, Text, StyleSheet, ScrollView, Image, TextInput, TouchableOpacity } from 'react-native';
-import JobCard from '../components/Jobcard';
+import JobCard from '../component/JOBCARDS.js';
 
 const HomeScreen = ({ route }) => {
   const { name, email } = route.params;
 
   const featuredJobs = [
-    { title: 'Software Engineer', company: 'Facebook', location: 'Accra, Ghana', salary: '$180,000', type: 'featured', image: require('assets/facebook.png') },
-    { title: 'Data Scientist', company: 'Google', location: 'Accra, Ghana', salary: '$160,000', type: 'featured', image: require('assets/google.png') },
-    { title: 'UI/UX Designer', company: 'Apple', location: 'San Francisco, US', salary: '$150,000', type: 'featured', image: require('assets/apple logo.png') },
-    { title: 'Backend Developer', company: 'Amazon', location: 'Seattle, US', salary: '$175,000', type: 'featured', image: require('assets/amazon.png') },
-    { title: 'Full Stack Developer', company: 'Microsoft', location: 'Redmond, US', salary: '$170,000', type: 'featured', image: require('assets/microsoft.png') },
-    { title: 'Data Analyst', company: 'Netflix', location: 'Los Gatos, US', salary: '$165,000', type: 'featured', image: require('../assets/netflix-logo.png') },
-    { title: 'DevOps Engineer', company: 'Spotify', location: 'Stockholm, Sweden', salary: '$160,000', type: 'featured', image: require('assets/spotify.png') },
-    { title: 'Mobile Developer', company: 'Snapchat', location: 'Los Angeles, US', salary: '$155,000', type: 'featured', image: require('../assets/snapchat-logo.png') },
-    { title: 'Machine Learning Engineer', company: 'Tesla', location: 'Palo Alto, US', salary: '$180,000', type: 'featured', image: require('assets/tesla.png') },
-    { title: 'Blockchain Developer', company: 'IBM', location: 'New York, US', salary: '$170,000', type: 'featured', image: require('assets/IBM.png') },
+    { title: 'Software Engineer', company: 'Facebook', location: 'Accra, Ghana', salary: '$180,000', type: 'featured', image: require('../assets/facebook.png') },
+    { title: 'Data Scientist', company: 'Google', location: 'Accra, Ghana', salary: '$160,000', type: 'featured', image: require('../assets/google.png') },
+    { title: 'UI/UX Designer', company: 'Apple', location: 'San Francisco, US', salary: '$150,000', type: 'featured', image: require('../assets/apple logo.png') },
+    { title: 'Backend Developer', company: 'Amazon', location: 'Seattle, US', salary: '$175,000', type: 'featured', image: require('../assets/amazon.png') },
+    { title: 'Full Stack Developer', company: 'Microsoft', location: 'Redmond, US', salary: '$170,000', type: 'featured', image: require('../assets/microsoft.png') },
+    { title: 'Data Analyst', company: 'Netflix', location: 'Los Gatos, US', salary: '$165,000', type: 'featured', image: require('../assets/netflix.png') },
+    { title: 'DevOps Engineer', company: 'Spotify', location: 'Stockholm, Sweden', salary: '$160,000', type: 'featured', image: require('../assets/spotify.png') },
+    { title: 'Mobile Developer', company: 'Snapchat', location: 'Los Angeles, US', salary: '$155,000', type: 'featured', image: require('../assets/snapchat.png') },
+    { title: 'Machine Learning Engineer', company: 'Tesla', location: 'Palo Alto, US', salary: '$180,000', type: 'featured', image: require('../assets/tesla.png') },
+    { title: 'Blockchain Developer', company: 'IBM', location: 'New York, US', salary: '$170,000', type: 'featured', image: require('../assets/IBM.png') },
   ];
 
   const popularJobs = [
-    { title: 'Jr Executive', company: 'Burger King', location: 'Los Angeles, US', salary: '$96,000/y', type: 'popular', image: require('../assets/burger-king-logo.png') },
-    { title: 'Product Manager', company: 'Beats', location: 'Florida, US', salary: '$84,000/y', type: 'popular', image: require('../assets/beats-logo.png') },
-    { title: 'Product Manager', company: 'Facebook', location: 'Florida, US', salary: '$86,000/y', type: 'popular', image: require('../assets/facebook-logo.png') },
-    { title: 'HR Manager', company: 'Spotify', location: 'Stockholm, Sweden', salary: '$90,000/y', type: 'popular', image: require('../assets/spotify-logo.png') },
-    { title: 'Marketing Specialist', company: 'Apple', location: 'San Francisco, US', salary: '$80,000/y', type: 'popular', image: require('../assets/apple-logo.png') },
-    { title: 'Financial Analyst', company: 'Netflix', location: 'Los Gatos, US', salary: '$92,000/y', type: 'popular', image: require('../assets/netflix-logo.png') },
-    { title: 'Software Engineer', company: 'Amazon', location: 'Seattle, US', salary: '$120,000/y', type: 'popular', image: require('../assets/amazon-logo.png') },
-    { title: 'Project Manager', company: 'Microsoft', location: 'Redmond, US', salary: '$110,000/y', type: 'popular', image: require('../assets/microsoft-logo.png') },
-    { title: 'Sales Manager', company: 'IBM', location: 'New York, US', salary: '$95,000/y', type: 'popular', image: require('../assets/ibm-logo.png') },
-    { title: 'Business Analyst', company: 'Tesla', location: 'Palo Alto, US', salary: '$100,000/y', type: 'popular', image: require('../assets/tesla-logo.png') },
+    { title: 'Jr Executive', company: 'Burger King', location: 'Los Angeles, US', salary: '$96,000/y', type: 'popular', image: require('../assets/burger king.png') },
+    { title: 'Product Manager', company: 'Beats', location: 'Florida, US', salary: '$84,000/y', type: 'popular', image: require('../assets/beats.png') },
+    { title: 'Product Manager', company: 'Facebook', location: 'Florida, US', salary: '$86,000/y', type: 'popular', image: require('../assets/facebook.png') },
+    { title: 'HR Manager', company: 'Spotify', location: 'Stockholm, Sweden', salary: '$90,000/y', type: 'popular', image: require('../assets/spotify.png') },
+    { title: 'Marketing Specialist', company: 'Apple', location: 'San Francisco, US', salary: '$80,000/y', type: 'popular', image: require('../assets/apple logo.png') },
+    { title: 'Financial Analyst', company: 'Netflix', location: 'Los Gatos, US', salary: '$92,000/y', type: 'popular', image: require('../assets/netflix.png') },
+    { title: 'Software Engineer', company: 'Amazon', location: 'Seattle, US', salary: '$120,000/y', type: 'popular', image: require('../assets/amazon.png') },
+    { title: 'Project Manager', company: 'Microsoft', location: 'Redmond, US', salary: '$110,000/y', type: 'popular', image: require('../assets/microsoft.png') },
+    { title: 'Sales Manager', company: 'IBM', location: 'New York, US', salary: '$95,000/y', type: 'popular', image: require('../assets/IBM.png') },
+    { title: 'Business Analyst', company: 'Tesla', location: 'Palo Alto, US', salary: '$100,000/y', type: 'popular', image: require('../assets/tesla.png') },
   ];
 
   return (
@@ -39,7 +39,7 @@ const HomeScreen = ({ route }) => {
           <Text style={styles.email}>{email}</Text>
         </View>
         <Image
-          source={require('assets/profile.jfif')}
+          source={require('../assets/profile.png')} // Replace with your local profile image
           style={styles.profileImage}
         />
       </View>
@@ -51,8 +51,8 @@ const HomeScreen = ({ route }) => {
         />
         <TouchableOpacity style={styles.filterButton}>
           <Image
-            source={require('assets/filter.png')}
-            style={{ width: 24, height: 24 }}
+            source={require('../assets/filter.png')} // Replace with your filter icon image
+            style={{ width: 24, height: 24 }} // Adjust dimensions as needed
           />
         </TouchableOpacity>
       </View>
